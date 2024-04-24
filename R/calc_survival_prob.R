963ef37ca5a2e18ec16c9971e00106dd38d9bfd4
javirudolph geog <javiera.av@ufl.edu>
2024-04-24 15:29:09 -0400

    modified function to get the param based on target survival

--- R/calc_survival_prob.R
+++ R/calc_survival_prob.R
@@ -6,6 +6,7 @@
 #' @param beta_0 vector or single value. The starting point for natural immunity. A higher value means greater natural immunity
 #' @param beta_1 vector or single value. The effect of the vaccine. Higher value means the vaccine has a greater positive effect on survival
 #' @param vax_rate value between 0 and 1, to represent the percentage of the population that is vaccinated
+#' @param target_zeta if defined, will return the value of beta_0 to get a specific survival probability under no vaccination
 #'
 #' @return description
 #' @export
@@ -13,6 +14,14 @@
 #' @examples
 #' calc_survival_prob(beta_0 = -1, beta_1 = 10, vax_rate = seq(0, 1, 0.01))
 #'
-calc_survival_prob <- function(beta_0, beta_1, vax_rate) {
-  (1 / (1 + exp(-(beta_0 + beta_1 * vax_rate))))
+calc_survival_prob <- function(beta_0, beta_1, vax_rate, target_zeta = NULL) {
+
+  if(is.null(target_zeta) == FALSE) {
+    out <- log(1/((1/target_zeta)-1))
+  } else {
+    out <- (1 / (1 + exp(-(beta_0 + beta_1 * vax_rate))))
+  }
+
+  return(out)
+
 }
